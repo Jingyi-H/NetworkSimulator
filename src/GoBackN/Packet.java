3b9f4bb02901f0d3f9384ff9630831cdf14ef65a
Alexstarwing <441698225@qq.com>
2021-10-31 19:28:48 -0400

    change packet form and add sack in it

--- src/GoBackN/Packet.java
+++ src/GoBackN/Packet.java
@@ -6,6 +6,7 @@ public class Packet
     private int acknum;
     private int checksum;
     private String payload;
+    private int[] sackNum = new int[5];
     
     public Packet(Packet p)
     {
@@ -13,6 +14,26 @@ public class Packet
         acknum = p.getAcknum();
         checksum = p.getChecksum();
         payload = new String(p.getPayload());
+        sackNum = p.getSackNum();
+    }
+
+    //new constructor of packet
+    public Packet(int seq, int ack, int check, String newPayload, int[] sackNum) {
+        seqnum = seq;
+        acknum = ack;
+        checksum = check;
+        if (newPayload == null)
+        {
+            payload = "";
+        }
+        else if (newPayload.length() > NetworkSimulator.MAXDATASIZE)
+        {
+            payload = null;
+        }
+        else
+        {
+            payload = new String(newPayload);
+        }
     }
     
     public Packet(int seq, int ack, int check, String newPayload)
@@ -99,6 +120,8 @@ public class Packet
     {
         return payload;
     }
+
+    public int[] getSackNum() { return sackNum; }
     
     public String toString()
     {
