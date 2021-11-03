8a9c05320bab05a3a800a0706b4551216c9c90de
Tina Hwang <tina_thedreamer@outlook.com>
2021-11-03 13:19:29 -0400

    add: statistics for stop n wait

--- src/SnwNetworkSimulator.java
+++ src/SnwNetworkSimulator.java
@@ -93,12 +93,22 @@ public class SnwNetworkSimulator extends NetworkSimulator
     private double RxmtInterval;
     private int LimitSeqNo;
     private int base_A;
+    private int base_B;
     private int nextSeqNo_A;
     private int nextSeqNo_B;
     private int currAck_A;
-    private int currAck_B;
+    private int lastAck_B;
+    private Packet currPkt;
     private ArrayList<Packet> buffer_A;
-//    private ArrayList<Packet> buffer_B;
+    private int maxBufferSize;
+
+    /* statistics variables */
+    private int rtxCnt = 0;
+    private int toLayer5Cnt = 0;
+    private int ackCnt = 0;
+    private int corruptedCnt = 0;
+    private int recvAcksCnt = 0;
+    private ArrayList<PacketStats> pktStats;
 
     // Add any necessary class variables here.  Remember, you cannot use
     // these variables to send messages error free!  They can only hold
@@ -130,14 +140,21 @@ public class SnwNetworkSimulator extends NetworkSimulator
     {
         String msgData = message.getData();
         // TODO: checksum calculation
-        int checksum = msgData.length();
         if (nextSeqNo_A > base_A + WindowSize) {
             return;
         }
+        int checksum = calcChecksum(msgData, nextSeqNo_A, currAck_A);
         Packet pkt = new Packet(nextSeqNo_A, currAck_A, checksum, msgData);
-        buffer_A.add(pkt);
+        if (buffer_A.size() <= maxBufferSize) {
+            buffer_A.add(pkt);
+            pktStats.add(new PacketStats(getTime()));
+        }
+        if (currPkt == null) {
+            currPkt = buffer_A.get(0);
+            buffer_A.remove(0);
+        }
         startTimer(A, 5 * RxmtInterval);
-        toLayer3(A, pkt);
+        toLayer3(A, currPkt);
         nextSeqNo_A = (nextSeqNo_A + 1) % LimitSeqNo;
     }
 
@@ -147,19 +164,35 @@ public class SnwNetworkSimulator extends NetworkSimulator
     // sent from the B-side.
     protected void aInput(Packet packet)
     {
-        if (packet.getChecksum() != packet.getPayload().length()) {
+        if (packet.getChecksum() != calcChecksum(packet)) {
             return;
         }
+
+        recvAcksCnt++;
+
         // A only receives ACKs
-        // ACK = next expected byte (i.e. nextSeqNo)
-        if (packet.getAcknum() != nextSeqNo_A) {
-            // resend packet from buffer
-            toLayer3(A, buffer_A.get(0));
+        if (packet.getAcknum() == base_A) {
+            // stop timer, get next packet from buffer
+            stopTimer(A);
+            double currTime = getTime();
+            if (buffer_A.size() > 0) {
+                currPkt = buffer_A.get(0);
+            }
+            else {
+                currPkt = null;
+            }
+            pktStats.get(getFirstUnACKed()).setAckTime(currTime);
+            base_A = (base_A + 1) % LimitSeqNo;
         }
-        // else -> stop timer, sws slides and clear buffer
-        stopTimer(A);
-        buffer_A.remove(0);
-        base_A = (base_A + 1) % LimitSeqNo;
+        else {
+            // resend current packet
+            stopTimer(A);
+            startTimer(A, 5 * RxmtInterval);
+            toLayer3(A, currPkt);
+            pktStats.get(getFirstUnACKed()).setRtx(true);
+            rtxCnt++;
+        }
+
     }
 
     // This routine will be called when A's timer expires (thus generating a
@@ -168,9 +201,14 @@ public class SnwNetworkSimulator extends NetworkSimulator
     // for how the timer is started and stopped.
     protected void aTimerInterrupt()
     {
-        // resend buffer
-        toLayer3(A, buffer_A.get(0));
+        stopTimer(A);
         startTimer(A, 5 * RxmtInterval);
+        if (currPkt == null) {
+            return;
+        }
+        toLayer3(A, currPkt);
+        rtxCnt++;
+        pktStats.get(getFirstUnACKed()).setRtx(true);
     }
 
     // This routine will be called once, before any of your other A-side
@@ -182,6 +220,9 @@ public class SnwNetworkSimulator extends NetworkSimulator
         base_A = nextSeqNo_A = FirstSeqNo;
         currAck_A = 0;
         buffer_A = new ArrayList<>();
+        currPkt = null;
+        maxBufferSize = 50;
+        pktStats = new ArrayList<>();
     }
 
     // This routine will be called whenever a packet sent from the B-side
@@ -193,26 +234,29 @@ public class SnwNetworkSimulator extends NetworkSimulator
         String recvData = packet.getPayload();
         // if packet is corrupted, drop it, send duplicate ACK to notify A
         // TODO: checksum calculation
-        if (packet.getChecksum() != recvData.length()) {
-            int checksum = 0;
-            Packet reAck = new Packet(nextSeqNo_B, packet.getSeqnum(), 0, "");
-            toLayer3(B, reAck);
+        if (packet.getChecksum() != calcChecksum(packet)) {
+            corruptedCnt++;
             return;
         }
         // if the packet is not the expected one, drop and resend ACK
         // currAck = nextExpectedSeqNo
-        if (packet.getSeqnum() != currAck_B) {
-            int checksum = 0;
-            Packet reAck = new Packet(nextSeqNo_B, packet.getSeqnum(), 0, "");
+        if (packet.getSeqnum() != base_B) {
+            int checksum = calcChecksum("", nextSeqNo_B, lastAck_B);
+            Packet reAck = new Packet(nextSeqNo_B, lastAck_B, checksum, "");
             toLayer3(B, reAck);
+            ackCnt++;
             return;
         }
         // receive packet successfully
-        Packet ack = new Packet(nextSeqNo_B, currAck_B, 0, "");
+        lastAck_B = (lastAck_B + 1) % LimitSeqNo;
+        base_B = (base_B + 1) % LimitSeqNo;
+        int checksum = calcChecksum("", nextSeqNo_B, lastAck_B);
+        Packet ack = new Packet(nextSeqNo_B, lastAck_B, checksum, "");
         toLayer3(B, ack);
-        currAck_B = (currAck_B + 1) % LimitSeqNo;
+        ackCnt++;
         // send data from B to upper layer
         toLayer5(recvData);
+        toLayer5Cnt++;
     }
 
     // This routine will be called once, before any of your other B-side
@@ -221,30 +265,138 @@ public class SnwNetworkSimulator extends NetworkSimulator
     // of entity B).
     protected void bInit()
     {
-        nextSeqNo_B = 1;
-        currAck_B = 0;
+        base_B = nextSeqNo_B = FirstSeqNo;
+        lastAck_B = LimitSeqNo - 1;
+    }
+
+    private int calcChecksum (String payload, int seqnum, int ack) {
+        int checkSum = 0;
+        for (int i = 0; i < payload.length(); i++) {
+            checkSum += (int)payload.charAt(i);
+        }
+        checkSum += seqnum + ack;
+
+        return checkSum;
+    }
+
+    private int calcChecksum (Packet packet) {
+        String payLoad = packet.getPayload();
+        int checkSum = 0;
+        for (int i = 0; i < payLoad.length(); i++) {
+            checkSum += (int)payLoad.charAt(i);
+        }
+        checkSum += packet.getSeqnum() + packet.getAcknum();
+
+        return checkSum;
+    }
+
+    protected int getFirstUnACKed() {
+        int i = 0;
+        while (i < pktStats.size()) {
+            if (!pktStats.get(i).isAcked()) {
+                break;
+            }
+            i++;
+        }
+        return i;
+    }
+
+    private double getAvgRTT() {
+        double rttSum = 0;
+        int rttCnt = 0;
+        for (PacketStats ps : pktStats) {
+            if (!ps.isRtx() && ps.isAcked()) {
+                rttCnt++;
+                rttSum += ps.getAckTime() - ps.getSendTime();
+            }
+        }
+        return rttSum / rttCnt;
+    }
+
+    private double getTtlRTT() {
+        double rttSum = 0;
+        for (PacketStats ps : pktStats) {
+            if (!ps.isRtx() && ps.isAcked()) {
+                rttSum += ps.getAckTime() - ps.getSendTime();
+            }
+        }
+        return rttSum;
+    }
+
+    private int getRttCnt() {
+        int rttCnt = 0;
+        for (PacketStats ps : pktStats) {
+            if (!ps.isRtx()  && ps.isAcked()) {
+                rttCnt++;
+            }
+        }
+        return rttCnt;
+    }
+
+    private double getAvgComms() {
+        double commsTime = 0;
+        int commsCnt = 0;
+        for (PacketStats ps : pktStats) {
+            if(ps.isAcked()) {
+                commsTime += ps.getAckTime() - ps.getSendTime();
+                commsCnt++;
+            }
+        }
+        return commsTime / commsCnt;
+    }
+
+    private double getTtlComms() {
+        double commsTime = 0;
+        for (PacketStats ps : pktStats) {
+            if(ps.isAcked())
+                commsTime += ps.getAckTime() - ps.getSendTime();
+        }
+        return commsTime;
+    }
+
+    private int getCommsCnt() {
+        int commsCnt = 0;
+        for (PacketStats ps : pktStats) {
+            if (!ps.isRtx()  && ps.isAcked()) {
+                commsCnt++;
+            }
+        }
+        return commsCnt;
     }
 
     // Use to print final statistics
     protected void Simulation_done()
     {
+        double rtt = getAvgRTT();
+        double comms = getAvgComms();
+        int originPktCnt = pktStats.size();
+        double lossRatio = (double)(rtxCnt - corruptedCnt) / (originPktCnt + rtxCnt + ackCnt);
+        double corruptedRatio = (double) corruptedCnt / (originPktCnt + rtxCnt + ackCnt - (rtxCnt - corruptedCnt));
+        double ttlRtt = getTtlRTT();
+        int rttCnt = getRttCnt();
+        double ttlComms = getTtlComms();
+        int commsCnt = getCommsCnt();
+
         // TO PRINT THE STATISTICS, FILL IN THE DETAILS BY PUTTING VARIBALE NAMES. DO NOT CHANGE THE FORMAT OF PRINTED OUTPUT
         System.out.println("\n\n===============STATISTICS=======================");
-        System.out.println("Number of original packets transmitted by A:" + "<YourVariableHere>");
-        System.out.println("Number of retransmissions by A:" + "<YourVariableHere>");
-        System.out.println("Number of data packets delivered to layer 5 at B:" + "<YourVariableHere>");
-        System.out.println("Number of ACK packets sent by B:" + "<YourVariableHere>");
-        System.out.println("Number of corrupted packets:" + "<YourVariableHere>");
-        System.out.println("Ratio of lost packets:" + "<YourVariableHere>" );
-        System.out.println("Ratio of corrupted packets:" + "<YourVariableHere>");
-        System.out.println("Average RTT:" + "<YourVariableHere>");
-        System.out.println("Average communication time:" + "<YourVariableHere>");
+        System.out.println("Number of original packets transmitted by A:" + originPktCnt);
+        System.out.println("Number of retransmissions by A:" + rtxCnt);
+        System.out.println("Number of data packets delivered to layer 5 at B:" + toLayer5Cnt);
+        System.out.println("Number of ACK packets sent by B:" + ackCnt);
+        System.out.println("Number of corrupted packets:" + corruptedCnt);
+        System.out.println("Ratio of lost packets:" + lossRatio);
+        System.out.println("Ratio of corrupted packets:" + corruptedRatio);
+        System.out.println("Average RTT:" + rtt);
+        System.out.println("Average communication time:" + comms);
         System.out.println("==================================================");
 
         // PRINT YOUR OWN STATISTIC HERE TO CHECK THE CORRECTNESS OF YOUR PROGRAM
         System.out.println("\nEXTRA:");
-        // EXAMPLE GIVEN BELOW
-        //System.out.println("Example statistic you want to check e.g. number of ACK packets received by A :" + "<YourVariableHere>");
+        System.out.println("number of non-corrupted ACKs received by A: " + recvAcksCnt);
+        System.out.println("All RTT: " + ttlRtt);
+        System.out.println("Counter RTT: " + rttCnt);
+        System.out.println("Total time to communicate: " + ttlComms);
+        System.out.println("Counter for time to communicate: " + commsCnt);
     }
 
 }
